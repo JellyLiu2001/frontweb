e9fdc1465dd718ac49a4863fb682483cd6e76f54
JellyLiu2001 <79320181+JellyLiu2001@users.noreply.github.com>
2023-02-16 22:44:35 +0000

    add my friends

---
+++ frontweb-main/import pandas.py
@@ -0,0 +1,7 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd  
+import numpy as np
+import xlrd
+print("12")
+#workbook=xlrd.open_workbook_xls("C:\\Users\\Jelly\\Desktop\\Big-Project-master\\T_data 数据汇总.xlsx")
